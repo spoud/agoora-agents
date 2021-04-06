7cf7c6a1f4daf36f669aff0c39e7d385e355d144
Gaétan Collaud <gaetancollaud@gmail.com>
2021-04-06 08:45:31 +0200

    add ccloud profile

--- agoora-agents-api/src/main/java/io/spoud/agoora/agents/api/config/AgooraAgentClientAuthConfig.java
+++ agoora-agents-api/src/main/java/io/spoud/agoora/agents/api/config/AgooraAgentClientAuthConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Builder
 @Data
--- agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/config/data/KafkaConfig.java
+++ agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/config/data/KafkaConfig.java
@@ -1,10 +1,12 @@
 package io.spoud.agoora.agents.kafka.config.data;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.Optional;
 
 @Data
+@ToString(exclude = {"loginSecret", "keyStorePassword", "trustStorePassword" })
 public class KafkaConfig {
 
   private String topicFilterRegex;
