1e35cd9a3f5acd37f61e81b2cf724c4f80ecf9b4
Gaétan Collaud <gaetancollaud@gmail.com>
2021-04-06 11:02:31 +0200

    review

--- agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/decoder/SampleDecoder.java
+++ agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/decoder/SampleDecoder.java
@@ -9,7 +9,6 @@ public interface SampleDecoder extends Comparable<SampleDecoder> {
 
   int getPriority();
 
-  // TODO should we use something different than string as output like bytes or a wrapper ?
   Optional<DecodedMessage> decode(String topic, KafkaStreamPart kafkaStreamPart, byte[] data)
       throws DecoderException;
 
