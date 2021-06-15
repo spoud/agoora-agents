7f3a2fd773334cbb25dcfef274bb9144162376ca
Gaétan Collaud <gaetancollaud@gmail.com>
2021-06-15 11:35:20 +0200

    code smell

--- agoora-agents-api/src/main/java/io/spoud/agoora/agents/api/client/MetricsClient.java
+++ agoora-agents-api/src/main/java/io/spoud/agoora/agents/api/client/MetricsClient.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
