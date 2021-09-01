4f74caa5afa6e710c2a7da55198f64927b9c10b6
Gaétan Collaud <gaetan.collaud@spoud.io>
2021-09-01 16:08:42 +0200

    improve log

--- agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/service/ProfilerService.java
+++ agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/service/ProfilerService.java
@@ -8,6 +8,7 @@ import io.spoud.agoora.agents.api.mapper.StandardProtoMapper;
 import io.spoud.agoora.agents.api.observers.ProfileResponseObserver;
 import io.spoud.agoora.agents.kafka.config.data.KafkaAgentConfig;
 import io.spoud.agoora.agents.kafka.data.KafkaTopic;
+import io.spoud.agoora.agents.kafka.decoder.DecoderException;
 import io.spoud.agoora.agents.kafka.decoder.DecoderService;
 import io.spoud.agoora.agents.kafka.kafka.KafkaTopicReader;
 import io.spoud.agoora.agents.kafka.repository.KafkaTopicRepository;
@@ -51,12 +52,17 @@ public class ProfilerService {
                 samples = decodeMessages(kafkaTopic, samples);
 
                 profileSamples(kafkaTopic, samples);
+              } catch (DecoderException ex){
+                LOG.warn("Unable to profile topic '{}' because we cannot decode it: {}", kafkaTopic.getTopicName(), ex.getMessage());
               } catch (Exception ex) {
-                LOG.error(
-                    "Unable to profile topic '{}', skipping. Enable debug for full stacktrace: {}",
-                    kafkaTopic.getTopicName(),
-                    ex.getMessage());
-                LOG.error("Unable to profile topic '{}'", kafkaTopic.getTopicName(), ex);
+                if(LOG.isDebugEnabled()) {
+                  LOG.error("Unable to profile topic '{}'", kafkaTopic.getTopicName(), ex);
+                }else{
+                  LOG.error(
+                          "Unable to profile topic '{}', skipping. Enable debug for full stacktrace: {}",
+                          kafkaTopic.getTopicName(),
+                          ex.getMessage());
+                }
 
                 lookerClient.addDataProfile(
                     AddDataProfileRequest.newBuilder()
