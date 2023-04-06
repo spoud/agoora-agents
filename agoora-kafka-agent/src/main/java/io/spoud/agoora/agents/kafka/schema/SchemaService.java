93ac9fd9aefa003bfa32b3d2e0864f43d611e668
Rui Teixeira <ruicrteixeira@gmail.com>
2023-04-06 16:57:58 +0200

    save key schema even when the value schema is undefined

--- agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/schema/SchemaService.java
+++ agoora-kafka-agent/src/main/java/io/spoud/agoora/agents/kafka/schema/SchemaService.java
@@ -35,8 +35,8 @@ public class SchemaService {
             .orElseGet(
                 () -> {
                   LOG.debug(
-                      "No schema found for topic {} in the registry {}", topicName,
-                      schemaRegistry.getDeepDiveToolUrl(topicName));
+                      "No {} schema found for topic {} in the registry {}", KafkaStreamPart.VALUE, topicName,
+                      schemaRegistry.getDeepDiveToolUrl(topicName).orElse("NOT_FOUND"));
                   return null;
                 });
 
@@ -45,8 +45,8 @@ public class SchemaService {
             .orElseGet(
                 () -> {
                   LOG.debug(
-                      "No schema found for topic {} in the registry {}", topicName,
-                      schemaRegistry.getDeepDiveToolUrl(topicName));
+                      "No {} schema found for topic {} in the registry {}", KafkaStreamPart.KEY, topicName,
+                      schemaRegistry.getDeepDiveToolUrl(topicName).orElse("NOT_FOUND"));
                   return null;
                 });
 
@@ -61,50 +61,57 @@ public class SchemaService {
   }
 
   private void saveSchema(String topicName, String dataPortId, Map<String, String> properties, SchemaRegistryClient schemaRegistry, Schema valueSchema, Schema keySchema) {
+    Type valueSchemaEncoding = Type.UNKNOWN;
+    String valueSchemaContent = "";
+    Type keySchemaEncoding = Type.UNKNOWN;
+    String keySchemaContent = "";
 
-    // TODO: accept schema with only KEY schema/encoding
+    if (valueSchema == null && keySchema == null) {
+      return;
+    }
+
     if (valueSchema != null) {
-      Type keySchemaEncoding = Type.UNKNOWN;
-      String keySchemaContent = "";
+      valueSchemaContent = valueSchema.getContent();
+      valueSchemaEncoding = valueSchema.getEncoding();
+    }
 
-      if (keySchema != null) {
-        keySchemaContent = keySchema.getContent();
-        keySchemaEncoding = keySchema.getEncoding();
-      }
+    if (keySchema != null) {
+      keySchemaContent = keySchema.getContent();
+      keySchemaEncoding = keySchema.getEncoding();
+    }
 
-      try {
-        LOG.debug(
-            "One KEY/VALUE schema found for the topic {} in the registry {}",
-            topicName,
-            schemaRegistry.getDeepDiveToolUrl(topicName));
+    try {
+      LOG.debug(
+          "One KEY/VALUE schema found for the topic {} in the registry {}",
+          topicName,
+          schemaRegistry.getDeepDiveToolUrl(topicName));
 
-        Schema saved =
-            schemaClient.saveSchema(
-                ResourceEntity.Type.DATA_PORT,
-                dataPortId,
-                kafkaAgentConfig
-                    .getTransport()
-                    .getAgooraPathObject()
-                    .getResourceGroupPath(),
-                valueSchema.getContent(),
-                SchemaSource.Type.REGISTRY,
-                valueSchema.getEncoding(),
-                keySchemaContent,
-                keySchemaEncoding);
+      Schema saved =
+          schemaClient.saveSchema(
+              ResourceEntity.Type.DATA_PORT,
+              dataPortId,
+              kafkaAgentConfig
+                  .getTransport()
+                  .getAgooraPathObject()
+                  .getResourceGroupPath(),
+              valueSchemaContent,
+              SchemaSource.Type.REGISTRY,
+              valueSchemaEncoding,
+              keySchemaContent,
+              keySchemaEncoding);
 
-        LOG.info("Successfully saved schema for topic {} with uuid '{}'", topicName,
-            saved.getId());
+      LOG.info("Successfully saved schema for topic {} with uuid '{}'", topicName,
+          saved.getId());
 
-        schemaRegistry
-            .getDeepDiveToolUrl(topicName)
-            .ifPresent(
-                url ->
-                    properties.put(
-                        Constants.PROPETIES_DEEP_DIVE_TOOL_SCHEMA_REGISTRY, url));
-      } catch (StatusRuntimeException ex) {
-        LOG.error("Unable to save value schema '{}' and key schema {} for topic {}", valueSchema,
-            keySchema, topicName, ex);
-      }
+      schemaRegistry
+          .getDeepDiveToolUrl(topicName)
+          .ifPresent(
+              url ->
+                  properties.put(
+                      Constants.PROPETIES_DEEP_DIVE_TOOL_SCHEMA_REGISTRY, url));
+    } catch (StatusRuntimeException ex) {
+      LOG.error("Unable to save value schema '{}' and key schema {} for topic {}", valueSchema,
+          keySchema, topicName, ex);
     }
   }
 }
